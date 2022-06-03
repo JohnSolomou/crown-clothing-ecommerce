8dcacd124164c19d1b62650041527e7d86157562
John Solomou <solomoufam@yahoo.com>
2022-06-02 21:51:37 -0400

    woked on firebase auth

--- src/components/routes/sign-in/sign-in.component.jsx
+++ src/components/routes/sign-in/sign-in.component.jsx
@@ -1,17 +1,23 @@
-import {
-  signInWithGooglePopup,
-  createUserDocumentFromAuth,
-} from "../../../utils/firebase/firebase.utils";
+// import { useEffect } from "react";
+// import { getRedirectResult } from "firebase/auth";
 
+import {
+  createUserDocumentFromAuth,
+  signInWithGooglePopup,
+} from "../../../utils/firebase/firebase.utils";
+import SignUpForm from "../../sign-up-form/sign-up-form.component";
 const SignIn = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
+    console.log(user, userDocRef);
   };
+
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google popup </button>
+      <SignUpForm />
     </div>
   );
 };
